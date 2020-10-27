c367049afbababc8227f257370b4d5223d1f2953
mfpDevForce <65236286+mfpDevForce@users.noreply.github.com>
2020-10-27 12:53:30 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -420,6 +420,359 @@ if (has_block('core/shortcode')) : ?>
     </script>
     <!-- END Back To Top script-->
 <?php endif; ?>
+
+<style>
+
+          #news-signup{
+              position: fixed;
+              width: 100%;
+              visibility: hidden;
+              z-index: 10002;
+              top: 100px;
+              opacity: 0;
+              -webkit-transform: scale(0.5);
+              transform: scale(0.5);
+              -webkit-transition: -webkit-transform 0.2s, opacity 0.2s, visibility 0s 0.2s;
+              transition: transform 0.2s, opacity 0.2s, visibility 0s 0.2s;
+          }
+          #news-signup .wrapper {
+              /*background: #252b33;*/
+              background: #948E99; 
+              background: -webkit-linear-gradient(to left, #252b33 , #2E1437);
+              background: linear-gradient(to left, #252b33 , #2E1437);
+              position: relative;
+              margin: 0 auto;
+              text-align: center;
+              -moz-border-radius: 3px;
+              box-shadow: 0px 1px 10px rgba(0,0,0,0.5);
+              width: 98%
+          }
+          #news-signup_bg {
+               visibility: hidden;
+               position: fixed;
+               top: 0;
+               left: 0;
+               width: 100%;
+               height: 100%;
+               background-color: #ffffff;
+               opacity: 0.4;
+               z-index: 10001;
+           }
+          #news-signup_close {
+              position: absolute;
+              left: 100%;
+              margin: 7px 0 0 -30px;
+              width: 20px;
+              height: 20px;
+              color: #fff;
+              opacity:0.3;
+              cursor: pointer;
+          }
+
+          #news-signup_close::before {
+              content: "×";
+              font: 400 26px/1em 'Roboto Slab', serif;
+          }
+
+          #news-signup_close:hover {
+              opacity:0.8;
+          }
+
+          #news-signup .newsletter-content {
+              padding: 100px 3px;
+              margin: 0 auto
+          }
+          #news-signup .newsletter-content h2 {
+               font: 300 24px/1em 'Roboto Slab', serif;
+              color: #fff;
+              text-align: center;
+              margin: 0 auto 15px
+          }
+          #news-signup .newsletter-content p {
+              margin: 0 auto 25px;
+              font: 300 16px/1em 'Roboto Slab', serif;
+              color: #7e8890
+          }
+          #news-signup .newsletter-content form {
+              margin: 0;
+              padding: 0;
+              width: 90%;
+          }
+
+          #news-signup .newsletter-content form p {
+              margin: 0 auto 10px
+          }
+
+          #news-signup .newsletter-content form p:not(.button) {
+              float: none;
+              width: 100%;
+          }
+
+          #news-signup .newsletter-content form p:not(.button) input {
+              width: 100%;
+              margin: 0;
+              padding: 12px 15px !important;
+              border-color: #d0d5d8
+          }
+
+          #news-signup .newsletter-content form .button {
+              float: none;
+              width: 100%
+          }
+
+          #news-signup .newsletter-content form .button input {
+              width: 100%;
+              padding: 20px 25px 18px !important;
+              font-size: 12px
+          }
+
+          #news-signup .newsletter-content p.footnote {
+              filter: progid: DXImageTransform.Microsoft.Alpha(Opacity=50);
+              opacity: 0.8;
+              font: 100 11px 'Roboto Slab', serif;
+              color: #abb0b7;
+              margin-bottom: 0
+          }
+
+          @media screen and (min-width: 800px){
+              #news-signup .wrapper {
+                  width: 800px;
+                  margin: 7% auto;
+              }
+          }
+
+
+</style>
+ 
+  <div id="news-signup">
+    <div class="wrapper">
+      <div id="news-signup_close"></div>
+      <div class="newsletter-content" id="phplistsubscriberesult">
+        <h3>We Have A special offer for you</h3>
+
+        <h2> 200 pokies in Jackpot City !</h2>
+        <p class="footnote">Give it a try. It only takes a click to unsubscribe.</p>
+      </div>
+    </div>
+  </div>
+ 
+
+ <script>
+ window.bioEp = {
+  // Private variables
+  bgEl: {},
+  popupEl: {},
+  closeBtnEl: {},
+  shown: false,
+  overflowDefault: "visible",
+
+  // Popup options
+  html: "",
+  css: "",
+  fonts: [],
+  delay: 2,
+  showOnDelay: false,
+  cookieExp: 30,
+
+  // Object for handling cookies, taken from QuirksMode
+  // https://www.quirksmode.org/js/cookies.html
+  cookieManager: {
+    // Create a cookie
+    create: function(name, value, days) {
+      var expires = "";
+
+      if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+      }
+
+      document.cookie = name + "=" + value + expires + "; path=/";
+    },
+
+    // Get the value of a cookie
+    get: function(name) {
+      var nameEQ = name + "=";
+      var ca = document.cookie.split(";");
+
+      for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == " ") c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+      }
+
+      return null;
+    },
+
+    // Delete a cookie
+    erase: function(name) {
+      this.create(name, "", -1);
+    }
+  },
+
+  // Handle the bioep_shown cookie
+  // If present and true, return true
+  // If not present or false, create and return false
+  checkCookie: function() {
+    // Handle cookie reset
+    if (this.cookieExp <= 0) {
+      this.cookieManager.erase("bioep_shown");
+      return false;
+    }
+
+    // If cookie is set to true
+    if (this.cookieManager.get("bioep_shown") == "true")
+      return true;
+
+    // Otherwise, create the cookie and return false
+    this.cookieManager.create("bioep_shown", "true", this.cookieExp);
+
+    return false;
+  },
+
+  // Add font stylesheets and CSS for the popup
+  addCSS: function() {
+    // Add font stylesheets
+    for (var i = 0; i < this.fonts.length; i++) {
+      var font = document.createElement("link");
+      font.href = this.fonts[i];
+      font.type = "text/css";
+      font.rel = "stylesheet";
+      font.rel = "stylesheet";
+      document.head.appendChild(font);
+    }
+
+  },
+
+  // Add the popup to the page
+  addPopup: function() {
+    // Add the background div
+    this.bgEl = document.createElement("div");
+    this.bgEl.id = "news-signup_bg";
+    document.body.appendChild(this.bgEl);
+
+    // Add the popup
+    if (document.getElementById("news-signup"))
+      this.popupEl = document.getElementById("news-signup");
+    else {
+      this.popupEl = document.createElement("div");
+      this.popupEl.id = "news-signup";
+      this.popupEl.innerHTML = this.html;
+      document.body.appendChild(this.popupEl);
+    }
+  },
+
+  // Show the popup
+  showPopup: function() {
+    if (this.shown) return;
+
+    this.bgEl.style.visibility = "visible";
+    this.popupEl.style.visibility = "visible";
+    this.popupEl.style.opacity = "1";
+    this.popupEl.style.transform = "scale(1)";
+    this.popupEl.style.webkitTransform = "scale(1)";
+    this.popupEl.style.transition = "0.4s, opacity 0.4s";
+    this.popupEl.style.webkitTransform = "0.4s, opacity 0.4s";
+    
+    // Save body overflow value and hide scrollbars
+    this.overflowDefault = document.body.style.overflow;
+    document.body.style.overflow = "hidden";
+
+    this.shown = true;
+  },
+
+  // Hide the popup
+  hidePopup: function() {
+    this.bgEl.style.visibility = "hidden";
+    this.popupEl.style.visibility = "hidden";
+    this.popupEl.style.opacity = "0";
+    this.popupEl.style.transform = "scale(0.5)";
+    this.popupEl.style.webkitTransform = "scale(0.5)";
+    this.popupEl.style.transition = "0.2s, opacity 0.2s, visibility 0s 0.2s";
+    this.popupEl.style.webkitTransform = "0.2s, opacity 0.2s, visibility 0s 0.2s";
+    document.body.style.overflow = this.overflowDefault;
+  },
+
+  // Event listener initialisation for all browsers
+  addEvent: function(obj, event, callback) {
+    if (obj.addEventListener)
+      obj.addEventListener(event, callback, false);
+    else if (obj.attachEvent)
+      obj.attachEvent("on" + event, callback);
+  },
+
+  // Load event listeners for the popup
+  loadEvents: function() {
+    // Track mouseout event on document
+    this.addEvent(document, "mouseout", function(e) {
+      e = e ? e : window.event;
+      var from = e.relatedTarget || e.toElement;
+
+      // Reliable, works on mouse exiting window and user switching active program
+      if (!from || from.nodeName === "HTML")
+        bioEp.showPopup();
+    });
+
+    // Handle the popup close button
+    this.closebtn = document.getElementById("news-signup_close");
+    this.addEvent(this.closebtn, "click", function() {
+      bioEp.hidePopup();
+    });
+  },
+
+  // Set user defined options for the popup
+  setOptions: function(opts) {
+    this.html = (typeof opts.html === 'undefined') ? this.html : opts.html;
+    this.css = (typeof opts.css === 'undefined') ? this.css : opts.css;
+    this.fonts = (typeof opts.fonts === 'undefined') ? this.fonts : opts.fonts;
+    this.delay = (typeof opts.delay === 'undefined') ? this.delay : opts.delay;
+    this.showOnDelay = (typeof opts.showOnDelay === 'undefined') ? this.showOnDelay : opts.showOnDelay;
+    this.cookieExp = (typeof opts.cookieExp === 'undefined') ? this.cookieExp : opts.cookieExp;
+  },
+
+  // Ensure the DOM has loaded
+  domReady: function(callback) {
+    (document.readyState === "interactive" || document.readyState === "complete") ? callback(): this.addEvent(document, "DOMContentLoaded", callback);
+  },
+
+  // Initialize
+  init: function(opts) {
+    // Handle options
+    if (typeof opts !== 'undefined')
+      this.setOptions(opts);
+
+    // Add CSS here to make sure user HTML is hidden regardless of cookie
+    // this.addCSS();
+
+    // Once the DOM has fully loaded
+    this.domReady(function() {
+      // Handle the cookie
+      if (bioEp.checkCookie()) return;
+
+      // Add the popup
+      bioEp.addPopup();
+
+      // Load events
+      setTimeout(function() {
+        bioEp.loadEvents();
+
+        if (bioEp.showOnDelay)
+          bioEp.showPopup();
+      }, bioEp.delay * 1000);
+    });
+  }
+}
+
+window.onload = function() {
+  document.getElementById("news_signup_email").focus();
+};
+
+bioEp.init({
+              //fonts: ['https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300'],
+              cookieExp: 0
+          });
+
+</script>
 </body>
 
 </html>
