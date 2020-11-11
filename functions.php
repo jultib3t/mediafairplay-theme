195df1bd98fb41d8383700d240dc046e93758de0
mfpDevForce <gal@mediafairplay.com>
2020-11-11 12:22:22 +0200

    update
    remove unnecessary head elemnts

--- functions.php
+++ functions.php
@@ -440,7 +440,6 @@ function wpex_wp_welcome_panel()
 
 
 
-
   // Remove jQuery Migrate Script from header and Load jQuery from Google API
 function crunchify_stop_loading_wp_embed_and_jquery() {
 	if (!is_admin()) {
@@ -449,6 +448,60 @@ function crunchify_stop_loading_wp_embed_and_jquery() {
 	}
 }
 add_action('init', 'crunchify_stop_loading_wp_embed_and_jquery');
+
+
+remove_action( 'wp_head', 'feed_links_extra', 3 ); // Display the links to the extra feeds such as category feeds
+remove_action( 'wp_head', 'feed_links', 2 ); // Display the links to the general feeds: Post and Comment Feed
+remove_action( 'wp_head', 'rsd_link' ); // Display the link to the Really Simple Discovery service endpoint, EditURI link
+remove_action( 'wp_head', 'wlwmanifest_link' ); // Display the link to the Windows Live Writer manifest file.
+remove_action( 'wp_head', 'index_rel_link' ); // index link
+remove_action( 'wp_head', 'parent_post_rel_link', 10, 0 ); // prev link
+remove_action( 'wp_head', 'start_post_rel_link', 10, 0 ); // start link
+remove_action( 'wp_head', 'adjacent_posts_rel_link', 10, 0 ); // Display relational links for the posts adjacent to the current post.
+remove_action( 'wp_head', 'wp_generator' );
+remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+remove_action( 'wp_print_styles', 'print_emoji_styles' );
+function disable_embeds_code_init() {
+
+  // Remove the REST API endpoint.
+  remove_action( 'rest_api_init', 'wp_oembed_register_route' );
+ 
+  // Turn off oEmbed auto discovery.
+  add_filter( 'embed_oembed_discover', '__return_false' );
+ 
+  // Don't filter oEmbed results.
+  remove_filter( 'oembed_dataparse', 'wp_filter_oembed_result', 10 );
+ 
+  // Remove oEmbed discovery links.
+  remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+ 
+  // Remove oEmbed-specific JavaScript from the front-end and back-end.
+  remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+  add_filter( 'tiny_mce_plugins', 'disable_embeds_tiny_mce_plugin' );
+ 
+  // Remove all embeds rewrite rules.
+  add_filter( 'rewrite_rules_array', 'disable_embeds_rewrites' );
+ 
+  // Remove filter of the oEmbed result before any HTTP requests are made.
+  remove_filter( 'pre_oembed_result', 'wp_filter_pre_oembed_result', 10 );
+ }
+ 
+ add_action( 'init', 'disable_embeds_code_init', 9999 );
+ 
+ function disable_embeds_tiny_mce_plugin($plugins) {
+     return array_diff($plugins, array('wpembed'));
+ }
+ 
+ function disable_embeds_rewrites($rules) {
+     foreach($rules as $rule => $rewrite) {
+         if(false !== strpos($rewrite, 'embed=true')) {
+             unset($rules[$rule]);
+         }
+     }
+     return $rules;
+ }
+// Remove dns-prefetch Link from WordPress Head (Frontend)
+remove_action( 'wp_head', 'wp_resource_hints', 2 );
 
 // toj custom rewrite
 function toj_custom_rewrites()
