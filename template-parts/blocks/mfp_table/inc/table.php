15482239deb0e5989c7c7ce5cdfec5cd074e610d
mfpDevForce <gal@mediafairplay.com>
2020-12-15 13:19:32 +0200

    update

--- template-parts/blocks/mfp_table/inc/table.php
+++ template-parts/blocks/mfp_table/inc/table.php
@@ -1,1 +1,9 @@
-<?php
+<?php ?>
+<div style="width: 100%; max-width: 700px;margin: 0 auto; text-align: center;">
+    <h2>Sorry This feature not excited right now.</h2>
+    <h4>What can you do?</h4>
+    <ol>
+        <li>Contact your king developer</li>
+    </ol>
+    <img style="width: 100%; max-width: 700px; height: auto;" src="https://media.giphy.com/media/3ohs4kgCYdVkJDhQGY/giphy.gif" />
+</div>
