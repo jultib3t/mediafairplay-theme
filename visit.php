1d33ec3693e63c8981c6e6f600abdc2bd1dbc76f
mfpDevForce <gal@mediafairplay.com>
2020-12-17 11:41:55 +0200

    update

--- visit.php
+++ visit.php
@@ -5,7 +5,9 @@ require_once("../../../wp-load.php");
 global $wpdb;
 // var_dump($_GET['visit_url']);
 $visit_url = str_replace('/','',$_GET['visit_url']);
- var_dump( $visit_url );
+$siteID = get_theme_mod('connect_your_site_to_aff_wiz');
+$geoID = get_theme_mod('connect_your_site_to_aff_wiz_id');
+ // var_dump( $visit_url );
 // $visit_url =   sanitize_text_field($_GET['visit_url']);
  // var_dump($visit_url);
 $website_id =   28; // TODO: Gal, you have to take it from DB
@@ -48,16 +50,14 @@ if(!empty($visit_url)) {
     // https://app.aff-wiz.com/wp-json/api/v1/getVisitURL?visit_url=all-slots-casino&website_id=4
     
     // $base_url = 'https://dev.theoffersjunction.com/wp-json/api/v1/getVisitURL?visit_url='.$visit_url.'&website_id=28';
-    $base_url = 'https://app.aff-wiz.com/wp-json/api/v1/getVisitURL?visit_url='.$visit_url.'&website_id=18';
+    $base_url = "https://app.aff-wiz.com/wp-json/api/v1/getVisitURL?visit_url={$visit_url}&website_id={$siteID}";
+    // var_dump( $base_url );
     $response = wp_remote_get($base_url);
    // echo '<img src="http://i.stack.imgur.com/SBv4T.gif" alt="this slowpoke moves"  width=250/>';
     if(!empty($response)) {
      //   echo '<img src="http://i.stack.imgur.com/SBv4T.gif" alt="this slowpoke moves"  width=250/>';
         $response   =   json_decode(wp_remote_retrieve_body($response));
-        if($response->status) { 
-           //  var_dump($response->tracker_url);
-            
-            ?>
+        if($response->status) { //  var_dump($response->tracker_url);  ?>
 
 <div class="wrapper">
     <img src="<?php echo get_template_directory_uri();?>/images/pokies-online-gif.gif" alt="this slowpoke moves"  width=250/>
